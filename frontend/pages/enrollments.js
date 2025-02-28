150261b089649300b4a518bed663306a724850ff
nikunj2019 <48497202+nikunj2019@users.noreply.github.com>
2025-02-28 04:02:29 +0000

    Fix

---
+++ frontend/pages/enrollments.js
@@ -0,0 +1,10 @@
+const Enrollments = () => {
+    return (
+      <div>
+        <h1>Enrollments Page</h1>
+        <p>This page will be used to manage enrollments.</p>
+      </div>
+    );
+  };
+  
+  export default Enrollments;  
---
+++ frontend/pages/upload.js
@@ -0,0 +1,10 @@
+const Upload = () => {
+    return (
+      <div>
+        <h1>Upload Page</h1>
+        <p>This page will be used to upload childcare data.</p>
+      </div>
+    );
+  };
+  
+  export default Upload;  
